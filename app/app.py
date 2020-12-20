04335d0d4c7e2cfad933e5403a9b9467e51f1aa9
SungminSo <soos196@hanyang.ac.kr>
2020-12-20 17:48:13 +0900

    add type hint

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ import falcon
 from .images import Resource, ImageStore
 
 
-def create_app(image_store):
+def create_app(image_store: str) -> falcon.API:
     api = falcon.API()
 
     api.add_route('/images', Resource(image_store=image_store))
@@ -11,6 +11,6 @@ def create_app(image_store):
     return api
 
 
-def get_app():
+def get_app() -> falcon.API:
     image_store = ImageStore(storage_path='storage')
     return create_app(image_store=image_store)
--- app/images.py
+++ app/images.py
@@ -8,14 +8,43 @@ import falcon
 import msgpack
 
 
+class ImageStore:
+
+    _CHUNK_SIZE_BYTES = 4096
+
+    # Note the use of dependency injection for standard library methods.
+    # We'll use these later to avoid monkey-patching.
+    def __init__(self, storage_path: str, uuidgen=uuid.uuid4, fopen=io.open):
+        self._storage_path = storage_path
+        self._uuidgen = uuidgen
+        self._fopen = fopen
+
+    def save(self, image_stream: falcon.Request.stream, image_content_type: str) -> str:
+        ext = mimetypes.guess_extension(image_content_type)
+        name = f'{self._uuidgen()}{ext}'
+        image_path = os.path.join(self._storage_path, name)
+
+        with self._fopen(image_path, 'wb') as image_file:
+            while True:
+                # By default Falcon does not spool or decode request data,
+                # instead giving you direct access to the incoming binary stream provide by the WSGI server.
+                chunk = image_stream.read(self._CHUNK_SIZE_BYTES)
+                if not chunk:
+                    break
+
+                image_file.write(chunk)
+
+        return name
+
+
 class Resource:
 
     # The resource object must now be initialized with a path used during POST
-    def __init__(self, image_store):
+    def __init__(self, image_store: ImageStore):
         self._image_store = image_store
 
     # If do '@staticmethod' -> "TypeError: on_get() missing 1 required positional argument: 'resp'"
-    def on_get(self, req, resp):
+    def on_get(self, req: falcon.Request, resp: falcon.Response):
         doc = {
             'images': [
                 {
@@ -33,36 +62,7 @@ class Resource:
         resp.content_type = falcon.MEDIA_MSGPACK
         resp.status = falcon.HTTP_OK
 
-    def on_post(self, req, resp):
+    def on_post(self, req: falcon.Request, resp: falcon.Response):
         name = self._image_store.save(req.stream, req.content_type)
         resp.status = falcon.HTTP_CREATED
         resp.location = '/images/' + name
-
-
-class ImageStore:
-
-    _CHUNK_SIZE_BYTES = 4096
-
-    # Note the use of dependency injection for standard library methods.
-    # We'll use these later to avoid monkey-patching.
-    def __init__(self, storage_path, uuidgen=uuid.uuid4, fopen=io.open):
-        self._storage_path = storage_path
-        self._uuidgen = uuidgen
-        self._fopen = fopen
-
-    def save(self, image_stream, image_content_type):
-        ext = mimetypes.guess_extension(image_content_type)
-        name = f'{self._uuidgen()}{ext}'
-        image_path = os.path.join(self._storage_path, name)
-
-        with self._fopen(image_path, 'wb') as image_file:
-            while True:
-                # By default Falcon does not spool or decode request data,
-                # instead giving you direct access to the incoming binary stream provide by the WSGI server.
-                chunk = image_stream.read(self._CHUNK_SIZE_BYTES)
-                if not chunk:
-                    break
-
-                image_file.write(chunk)
-
-        return name
